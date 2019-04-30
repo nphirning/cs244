7eb63148d258aeceed3fed7502ed80e437c9b80e
Michael Straka <michaelstraka@DN0a246779.SUNet>
2019-04-29 17:46:05 -0700

    changed script

--- assign2/run.sh
+++ assign2/run.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 
-
+rm -rf results
+mkdir results
+for i in {1..5}
+do
 	cat ./sites.csv | while read line
 	do
 		sudo python3 ./icw-test.py $line >> "./results/$line.txt"
 	done
-	#python3 ./data-count.py > "./summary.txt"
-
+done
+python3 ./data-count.py > "./summary.txt"
